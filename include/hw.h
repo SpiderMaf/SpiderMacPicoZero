ae79cd47b1b1b28663b0a279fcee52121d113b16
Matthew Norman <matnorman@gmail.com>
2024-10-12 17:48:42 +0100

    Added modified hw.h

---
+++ include/hw.h
@@ -0,0 +1,40 @@
+/*
+ * pico-umac pin definitions
+ *
+ * Copyright 2024 Matt Evans
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef HW_H
+#define HW_H
+
+/*The following are modified from the default build to use this with SpiderMacPico (and RO2040-Zero) boards.*/
+
+
+#define GPIO_LED_PIN    28
+
+#define GPIO_VID_DATA   9
+#define GPIO_VID_VS     10
+#define GPIO_VID_CLK    11
+#define GPIO_VID_HS     12
+
+#endif
